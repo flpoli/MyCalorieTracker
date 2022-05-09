b730158552a474af2ff54e5abf07235c591679eb
Felipe Policisse <policisse@gmail.com>
2022-05-08 22:11:39 -0300

    age screen and viewmodel

--- onboarding/onboarding_presentation/src/main/java/com/poli/onboarding_presentation/welcome/WelcomeScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/poli/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -44,7 +44,7 @@ fun WelcomeScreen(
 
         ActionButton(
             text = stringResource(R.string.next),
-            onClick = {onNavigate(UiEvent.Navigate(Route.AGE))},
+            onClick = {onNavigate(UiEvent.Navigate(Route.GENDER))},
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
     }
