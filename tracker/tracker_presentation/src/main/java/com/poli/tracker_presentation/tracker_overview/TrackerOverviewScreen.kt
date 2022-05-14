bd1b41f61259e00366bc32669c4e0302fde97b7c
Felipe Policisse <policisse@gmail.com>
2022-05-14 20:36:47 -0300

    Day selector

--- tracker/tracker_presentation/src/main/java/com/poli/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/poli/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -1,6 +1,9 @@
 package com.poli.tracker_presentation.tracker_overview
 
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
@@ -9,6 +12,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.poli.core.util.UiEvent
 import com.poli.core_ui.LocalSpacing
+import com.poli.tracker_presentation.tracker_overview.components.DaySelector
 import com.poli.tracker_presentation.tracker_overview.components.NutrientsHeader
 
 @Composable
@@ -29,6 +33,17 @@ fun TrackerOverviewScreen(
 
         item {
             NutrientsHeader(state = state)
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+
+            DaySelector(
+                date = state.date,
+                onNextDayClick = { viewModel.onEvent(TrackerOverViewEvent.OnPreviousDayClick) },
+                onPreviousDayClick = { viewModel.onEvent(TrackerOverViewEvent.OnNextDayClick) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = spacing.spaceMedium)
+            )
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
         }
     }
 
